32d83d69664ef46c7aa0e5a92efb29e9bc893eb8
TheWellz <welltongabrielkkj19@gmail.com>
2025-03-06 12:34:49 -0300

    .

--- provedor/webmail/config.inc.php
+++ provedor/webmail/config.inc.php
@@ -2,24 +2,24 @@
 
 $config['db_dsnw'] = 'sqlite:////var/www/html/roundcubemail.sqlite';  // Usando SQLite para armazenar as configurações
 $config['default_host'] = 'imaps://mail.rosado.com';  // Servidor IMAP (Dovecot)
-$config['smtp_server'] = 'smtp://mail.rosado.com';  // Servidor SMTP (Postfix)
+$config['smtp_server'] = 'localhost'; 
 $config['smtp_port'] = 25;  // Porta do SMTP (configurada no Postfix)
 $config['smtp_user'] = '%u';  // Nome de usuário SMTP (será substituído pelo usuário autenticado)
 $config['smtp_pass'] = '%p';  // Senha do usuário SMTP
-$config['imap_conn_options'] = array(
-    'ssl' => array(
+$config['imap_conn_options'] = [
+    'ssl' => [
         'verify_peer' => false,
         'verify_peer_name' => false,
         'allow_self_signed' => true
-    )
-);
-$config['smtp_conn_options'] = array(
-    'ssl' => array(
+    ]
+];
+$config['smtp_conn_options'] = [
+    'ssl' => [
         'verify_peer' => false,
         'verify_peer_name' => false,
         'allow_self_signed' => true
-    )
-);
+    ]
+];
 
 $config['log_driver'] = 'syslog';  // Configuração de logs
 $config['enable_installer'] = false;  // Desabilita o instalador do Roundcube
